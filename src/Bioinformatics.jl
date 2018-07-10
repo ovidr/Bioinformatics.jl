9fb199c22e86b1d2df10163f99db949432561a72
Murat Koptur <mkoptur3@gmail.com>
2018-07-10 18:28:21 +0300

    first commit, travis configured

---
+++ src/Bioinformatics.jl
@@ -0,0 +1,55 @@
+module Bioinformatics
+export verifyDna, readStringFromFile, countBases, transcribeDna
+
+const dnaAlphabet = ['A', 'C', 'G', 'T']
+const rnaAlphabet = ['A', 'C', 'G', 'U']
+
+function verifyDna(dna::String)
+    if all(base -> base in dnaAlphabet, dna)
+        return true
+    else
+        error("String is not a valid DNA string!")
+    end
+end
+
+function readStringFromFile(filename::String)
+    s = open(filename) do f
+        read(f, String)
+    end
+    return strip(s)
+end
+
+function countBases(dna::String)
+    countA = 0
+    countC = 0
+    countG = 0
+    countT = 0
+    for c in dna
+        if c == 'A'
+            countA += 1
+        elseif c == 'C'
+            countC += 1
+        elseif c == 'G'
+            countG += 1
+        elseif c == 'T'
+            countT += 1
+        else
+            error("Char $c is not a valid base!")
+        end
+    end
+    return countA, countC, countG, countT
+end
+
+function transcribeDna(dna::String)
+    rna = ""
+    for c in dna
+        if c == 'T'
+            rna = string(rna, 'U')
+        else
+            rna = string(rna, c)
+        end
+    end
+    return rna
+end
+
+end
---
+++ tests/runtests.jl
@@ -0,0 +1,12 @@
+include("../src/Bioinformatics.jl")
+using Bioinformatics
+if VERSION < v"0.7.0-DEV.2005"
+    using Base.Test
+else
+    using Test
+end
+
+@test verifyDna("ACGT") == true
+@test_throws ErrorException verifyDna("ACFY") == false
+@test countBases("AAGCCCT") == (2, 3, 1, 1)
+@test transcribeDna("ACGT") == "ACGU"
