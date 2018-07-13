bed52fee1a41670b612a0017adf16f1c89f9d290
Murat Koptur <mkoptur3@gmail.com>
2018-07-13 19:20:18 +0300

    findMotif, translate, profileMatrix, consensusString functions added

--- src/Bioinformatics.jl
+++ src/Bioinformatics.jl
@@ -6,12 +6,42 @@ export verifyDna,
        reverseComplement,
        readFASTA,
        gcContent,
-       hammingDist
+       hammingDist,
+       translateRNA,
+       findMotif,
+       profileMatrix,
+       consensusString
 
 const dnaAlphabet = ['A', 'C', 'G', 'T']
 const rnaAlphabet = ['A', 'C', 'G', 'U']
 
 const dnaComplements = Dict('A' => 'T', 'T' => 'A', 'C' => 'G', 'G' => 'C')
+
+const proteinAlphabet = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L',
+                         'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
+
+const rnaCodonTable = Dict("UUU" => 'F',      "CUU" => 'L',      "AUU" => 'I',
+                           "GUU" => 'V',	  "UUC" => 'F',      "CUC" => 'L',
+                           "AUC" => 'I',      "GUC" => 'V',      "UUA" => 'L',
+                           "CUA" => 'L',      "AUA" => 'I',      "GUA" => 'V',
+                           "UUG" => 'L',      "CUG" => 'L',      "AUG" => 'M',
+                           "GUG" => 'V',      "UCU" => 'S',      "CCU" => 'P',
+                           "ACU" => 'T',      "GCU" => 'A',      "UCC" => 'S',
+                           "CCC" => 'P',      "ACC" => 'T',      "GCC" => 'A',
+                           "UCA" => 'S',      "CCA" => 'P',      "ACA" => 'T',
+                           "GCA" => 'A',      "UCG" => 'S',      "CCG" => 'P',
+                           "ACG" => 'T',      "GCG" => 'A',      "UAU" => 'Y',
+                           "CAU" => 'H',      "AAU" => 'N',      "GAU" => 'D',
+                           "UAC" => 'Y',      "CAC" => 'H',      "AAC" => 'N',
+                           "GAC" => 'D',      "UAA" => "Stop",   "CAA" => 'Q',
+                           "AAA" => 'K',      "GAA" => 'E',      "UAG" => "Stop",
+                           "CAG" => 'Q',      "AAG" => 'K',      "GAG" => 'E',
+                           "UGU" => 'C',      "CGU" => 'R',      "AGU" => 'S',
+                           "GGU" => 'G',      "UGC" => 'C',      "CGC" => 'R',
+                           "AGC" => 'S',      "GGC" => 'G',      "UGA" => "Stop",
+                           "CGA" => 'R',      "AGA" => 'R',      "GGA" => 'G',
+                           "UGG" => 'W',      "CGG" => 'R',      "AGG" => 'R',
+                           "GGG" => 'G')
 
 """
     verifyDna(dna::String)
@@ -168,6 +198,89 @@ function hammingDist(s::String, t::String)
         end
     end
     return dist
+end
+
+"""
+    translateRNA(rna::String)
+
+Translates a RNA string to protein string.
+"""
+function translateRNA(rna::String)
+    proteinString = ""
+    for i = 1:3:length(rna)-2
+        aa = rnaCodonTable[rna[i:i+2]]
+        if aa == "Stop"
+            continue
+        else
+            proteinString = string(proteinString, aa)
+        end
+    end
+    return proteinString
+end
+
+"""
+    findMotif(s::String, t::String)
+
+Returns all locations of t as a substring of s.
+"""
+function findMotif(s::String, t::String)
+  indexes = []
+  for i = 1:(length(s) - length(t) + 1)
+    if s[i:i+length(t) - 1] == t
+      append!(indexes, i)
+    end
+  end
+  return indexes
+end
+
+"""
+    profileMatrix(dna::Dict)
+
+Calculates profile matrix of a collection of DNA strings.
+"""
+function profileMatrix(dna::Dict)
+  colSize = length(collect(Iterators.take(dna, 1))[1][2])
+  profile = zeros(4, colSize)
+  for k in keys(dna)
+    str = (dna[k])
+    for i = 1:length(str)
+      if str[i] == 'A'
+        profile[1, i] += 1
+      elseif str[i] == 'C'
+        profile[2, i] += 1
+      elseif str[i] == 'G'
+        profile[3, i] += 1
+      else
+        profile[4, i] += 1
+      end
+    end
+  end
+  return profile
+end
+
+"""
+    consensusString(profileMatrix::Array)
+
+Calculates consensus string from a profile matrix.
+"""
+function consensusString(profileMatrix::Array)
+  rowSize, colSize = size(profileMatrix)
+  consensus = ""
+  for i = 1:colSize
+    b = ' '
+    ind = indmax(profileMatrix[:, i])
+    if ind == 1
+      b = 'A'
+    elseif ind == 2
+      b = 'C'
+    elseif ind == 3
+      b = 'G'
+    else
+      b = 'T'
+    end
+    consensus = string(consensus, b)
+  end
+  return consensus
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -15,3 +15,17 @@ end
 @test round(gcContent("CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGACTGGGAACCTGCGGGCAGTAGGTGGAAT"), 2) == 60.92
 @test gcContent(Dict("DNA_1" => "AGTC", "DNA_2" => "AATG")) == Dict("DNA_1" => 50.0, "DNA_2" => 25.0)
 @test hammingDist("GAGCCTACTAACGGGAT", "CATCGTAATGACGGCCT") == 7
+@test translateRNA("AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA") == "MAMAPRTEINSTRING"
+@test findMotif("GATATATGCATATACTT", "ATAT") == [2, 4, 10]
+@test profileMatrix(Dict("Rosalind_1" => "ATCCAGCT", "Rosalind_2" => "GGGCAACT",
+                         "Rosalind_3" => "ATGGATCT", "Rosalind_4" => "AAGCAACC",
+                         "Rosalind_5" => "TTGGAACT", "Rosalind_6" => "ATGCCATT",
+                         "Rosalind_7" => "ATGGCACT")) == 
+                                    [[5.0  1.0  0.0  0.0  5.0  5.0  0.0  0.0]
+                                     [0.0  0.0  1.0  4.0  2.0  0.0  6.0  1.0]
+                                     [1.0  1.0  6.0  3.0  0.0  1.0  0.0  0.0]
+                                     [1.0  5.0  0.0  0.0  0.0  1.0  1.0  6.0]]
+@test consensusString([[5.0  1.0  0.0  0.0  5.0  5.0  0.0  0.0]
+                       [0.0  0.0  1.0  4.0  2.0  0.0  6.0  1.0]
+                       [1.0  1.0  6.0  3.0  0.0  1.0  0.0  0.0]
+                       [1.0  5.0  0.0  0.0  0.0  1.0  1.0  6.0]]) == "ATGCAACT"
